b9e49fa18f7aabd28573cd8468bfce75eae9035c
Icaro Araujo <icaroasa99@gmail.com>
2023-06-01 06:55:30 -0300

    Category

--- src/main/java/com/educandoweb/course/services/OrderService.java
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -11,16 +11,16 @@ import com.educandoweb.course.repositories.OrderRepository;
 
 @Service
 public class OrderService {
-	
+
 	@Autowired
-	private OrderRepository userRepository;
-	
-	public List<Order> findAll(){
-		return userRepository.findAll();
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
 	}
-	
+
 	public Order findById(Long id) {
-		Optional<Order> obj = userRepository.findById(id);
+		Optional<Order> obj = repository.findById(id);
 		return obj.get();
 	}
 }
